4139db5921a574dc51ebb00176756ac6740c3d0d
Fahim-ok <79527791+Fahim-ok@users.noreply.github.com>
2021-09-27 21:00:15 +0600

    submit

---
+++ mcq_eng.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ENGLISH MCQ</title>
+    <style>
+     h1{
+         background-color:teal;
+         text-align:center;
+         color:white;
+         padding:10px;
+         font-size:50px;
+     }
+     input[type="radio"]{
+         padding-right: 15px;
+     }
+     div{
+         margin-left:550px;
+     }
+     p{
+         font-weight:bold;
+         font-size:22px;
+     }
+    </style>
+</head>
+<body>
+<h1>MCQ</h1>
+<div>
+<form action="mcq_eng2.php" method="post">
+     <p>1. "About to" is a special structure that is used to talk about -</p>
+     A. <input type="radio" name="q1" value="The immediate future" required> The immediate future<br>
+     B. <input type="radio" name="q1" value="The immediate present" required> The immediate present <br>
+     C. <input type="radio" name="q1" value="The immediate past" required>The immediate past <br>
+     D. <input type="radio" name="q1" value="The past" required>The past <br> <br>
+     <p>2. How many vowels are there in the English alphabet?</p>
+     A. <input type="radio" name="q2" value="Five" required> Five <br>
+     B. <input type="radio" name="q2" value="Ten" required> Ten <br>
+     C. <input type="radio" name="q2" value="Twenty" required> Twenty <br>
+     D. <input type="radio" name="q2" value="Three" required> Three <br> <br>
+     <p>3. The basic structure of a question is : "auxiliary verb + subject	+ main verb". Pick the correct sentence that satisfies this structure.</p>
+     A. <input type="radio" name="q3" value="Does like him you?" required> Does like him you? <br>
+     B. <input type="radio" name="q3" value="Do you like him?" required> Do you like him? <br>
+     C. <input type="radio" name="q3" value="What dog doing?" required> What dog doing? <br>
+     D. <input type="radio" name="q3" value="Happened here what?" required> Happened here what? <br><br>
+     <input type="submit" name="submit" value="Submit">
+</form>
+</div> 
+</body>
+</html>
+
