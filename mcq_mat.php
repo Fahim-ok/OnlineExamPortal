9a75d3e6d3bb9834d98ba013f3ab3793e86daedd
Fahim-ok <79527791+Fahim-ok@users.noreply.github.com>
2021-09-27 21:02:36 +0600

    submit

---
+++ mcq_mat.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>MATH MCQ</title>
+    <style>
+     h1{
+         background-color:teal;
+         text-align:center;
+         color:white;
+         padding:10px;
+         font-size:50px;
+     }
+     input[type="radio"]{
+         padding-right: 15px;
+     }
+     div{
+         margin-left:550px;
+     }
+     p{
+         font-weight:bold;
+         font-size:22px;
+     }
+    </style>
+</head>
+<body>
+<h1>MCQ</h1>
+<div>
+<form action="mcq_mat2.php" method="post">
+     <p>1. What is 9 + 10?</p>
+     A. <input type="radio" name="q1" value="15" required>15 <br>
+     B. <input type="radio" name="q1" value="21" required>21  <br>
+     C. <input type="radio" name="q1" value="19" required>19 <br>
+     D. <input type="radio" name="q1" value="67" required>67 <br> <br>
+     <p>2. What is next prime number after 7?</p>
+     A. <input type="radio" name="q2" value="69" required> 69 <br>
+     B. <input type="radio" name="q2" value="15" required> 15 <br>
+     C. <input type="radio" name="q2" value="12" required> 12 <br>
+     D. <input type="radio" name="q2" value="11" required> 11 <br> <br>
+     <p>3. Calculate: 121/11 (121 divided by 11).</p>
+     A. <input type="radio" name="q3" value="42" required> 42 <br>
+     B. <input type="radio" name="q3" value="15" required> 15 <br>
+     C. <input type="radio" name="q3" value="11" required> 11 <br>
+     D. <input type="radio" name="q3" value="56" required> 56 <br><br>
+     <input type="submit" name="submit" value="Submit">
+</form>
+</div>    
+</body>
+</html>
+
