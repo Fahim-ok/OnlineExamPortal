d421b4af93e483d310dbf45c1538a23c90868d6a
Fahim-ok <79527791+Fahim-ok@users.noreply.github.com>
2021-09-27 20:59:37 +0600

    submit

---
+++ mcq_che.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Chemistry MCQ</title>
+    <style>
+     h1{
+         background-color:teal;
+         text-align:center;
+         color:white;
+         padding:10px;
+         font-size:50px;
+     }
+     input[type="radio"]{
+         padding-right: 15px;
+     }
+     div{
+         margin-left:550px;
+     }
+     p{
+         font-weight:bold;
+         font-size:22px;
+     }
+    </style>
+</head>
+<body>
+<h1>MCQ</h1>
+<div>
+<form action="mcq_che2.php" method="post">
+     <p>1.Which metal remains in the liquid from under normal conditions?</p>
+     A. <input type="radio" name="q1" value="Mercury" required>Mercury <br>
+     B. <input type="radio" name="q1" value="Zinc" required>Zinc  <br>
+     C. <input type="radio" name="q1" value="Radium" required>Radium <br>
+     D. <input type="radio" name="q1" value="Calcium" required> Calcium<br> <br>
+     <p>2.Which of the following is the source of common salt?</p>
+     A. <input type="radio" name="q2" value="Bauxite" required> Bauxite <br>
+     B. <input type="radio" name="q2" value="Halite" required> Halite <br>
+     C. <input type="radio" name="q2" value="Calcite" required> Calcite <br>
+     D. <input type="radio" name="q2" value="Pyrite" required> Pyrite <br> <br>
+     <p>3.The gas that is responsible for global warming is...?</p>
+     A. <input type="radio" name="q3" value="Methane" required> Methane <br>
+     B. <input type="radio" name="q3" value="Oxygen" required> Oxygen <br>
+     C. <input type="radio" name="q3" value="Sulphur" required> Sulphur <br>
+     D. <input type="radio" name="q3" value="Carbon Dioxide" required> Carbon Dioxide <br><br>
+     <input type="submit" name="submit" value="Submit">
+</form>
+</div>    
+</body>
+</html>
+
